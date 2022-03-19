fbbeaa414035a40240c6c6f13fead5ecf9547405
matte <mattedwards1452@gmail.com>
2022-03-18 23:19:57 -0700

    probably broke it

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ class Warrior extends Player {
         this.spells = [];
     }
     swingWeapon() {
-        console.log("we swingin")
+        console.log("we swingin!")
     }
 }
 //set up monster
@@ -31,15 +31,16 @@ class Monster {
     this.items = [];
 }
     swingWeapon() {
-    console.log("we swingin")
+    console.log("we swingin!")
 }
 }
 /*----- app's state (variables) -----*/
 //define a basic player and basic monster
-let player;
-let badGuy;
+
 let newBadHealth = 50;
 let newGoodHealth = 75;
+let attackResultGood = "";
+let attackResultBad = "";
 
 
 /*----- cached element references -----*/
@@ -47,6 +48,8 @@ const  badGuySprite = document.querySelector('.badGuySprite');
 const  goodGuySprite = document.querySelector('.goodGuySprite');
 const  badGuyHealth = document.querySelector('#badGuyHealthMeter');
 const  goodGuyHealth = document.querySelector('#goodGuyHealthMeter');
+const  attackResultDisplayBad = document.querySelector('.attackResultDisplayBad');
+const  attackResultDisplayGood = document.querySelector('.attackResultDisplayGood');
 
 /*----- event listeners -----*/
 //main bar for attacking etc.
@@ -55,16 +58,24 @@ document.querySelector('.inputBar').addEventListener('click', handleClick);
 document.querySelector('nav').addEventListener('click', handleNav);
 
 /*----- functions -----*/
+let player = new Warrior("Matt","Sword",10,100,"","");
+let badGuy = new Monster("Little Demon",50,10,0,100,"");
 
-function init() {
-    const player = new Warrior("Matt","Sword",10,100,"","")
-    const badGuy = new Monster("Little Demon",50,10,0,100,"")
-}
-init();
+// function init() {
+//     let player = new Warrior("Matt","Sword",10,100,"","");
+//     let badGuy = new Monster("Little Demon",50,10,0,100,"");
+    
+// }
+// init();
+console.log(Player.name)
+console.log(player)
+console.log(badGuy)
 
 function render(){
     badGuyHealth.value = newBadHealth;
-    goodGuyHealth.value = newGoodHealth
+    goodGuyHealth.value = newGoodHealth;
+    attackResultDisplayBad.innerText = attackResultBad
+    attackResultDisplayGood.innerText = attackResultGood
 }
 
 function handleClick(e) {
@@ -80,9 +91,41 @@ function handleClick(e) {
         }
 render();    
 }};
+//make an attack roll, check to see if it hits, if it does, then roll damage in a sperate function.  4 result (crit, hit/player block, both hit, player only hit)
 function doAttack(e) {
+    const dieRoll = Math.random()
+    if (dieRoll >.9) {
+        attackResultGood = "Critical Hit!!";
+        attackResultBad = "OUCH!!!";
+        doDamage(2);
+    } else if (dieRoll > 6) {
+        attackResultGood = "Hit!";
+        attackResultBad = "Swing, blocked!";
+        doDamage(1);
+    } else if (dieRoll >.2) {
+        attackResultGood = "Hit!";
+        attackResultBad = "Hit!";
+        doDamage(1);
+        takeDamage(1);
+    } else if (dieRoll >.05) {
+        attackResultGood = "I miss!!";
+        attackResultBad = "Hit!";
+        takeDamage(1);
+    } else {
+        attackResultGood = "I miss!!";
+        attackResultBad = "Critical Hit!";
+        takeDamage(2);
+    }
+    
+render()    
+}
+function doDamage(e) {
+
 
 render()    
+}
+function takeDamage(e) {
+
 }
 function doRun(e) {
 
